f29f6983e42bbb592ec47f4705c0fad2aba78cff
dcabel02 <51374220+dcabel02@users.noreply.github.com>
2024-01-08 21:30:38 -0500

    updates to calculations and comments

--- bdb-HustleMetric.R
+++ bdb-HustleMetric.R
@@ -25,36 +25,67 @@ trackingNew <- tracking %>%
   mutate(oAdj = ifelse(playDirection == "left", 180+o, o)%%360) %>%
   dplyr::select(-c(x, y, dir, o))
 
-### Isolating ball carrier ###
-ballCarrier <- trackingNew %>%
-  filter(event == "tackle") %>%
-  inner_join(plays, by = c("gameId", "playId")) %>%
-  filter(ballCarrierId == nflId) %>%
-  dplyr::select(ballCarrierId, ballCarrierDisplayName, xAdj, yAdj, gameId, playId)
+### Isolating all RB and WR's ###
+runningbacks <- players %>%
+  filter(position == "RB" | position == "WR")
 
-### Updating x and y column names so it's easier to identify when added to other tracking data sets ###
-colnames(ballCarrier)[3:4] <- c("ballCarrierX", "ballCarrierY")
+### Isolating all run plays ###
+runPlays <- plays %>%
+  filter(passResult == "R" | (ballCarrierId %in% runningbacks$nflId & passResult != "C"))
 
-### Filtering all plays that end in tackle and only keeping last frame ###
-tackling <- trackingNew %>%
-  filter(event == "tackle") %>%
-  inner_join(plays, by = c("gameId", "playId")) %>%
+### Isolating ball carrier tracking data to add to other tracking data later ###
+trackingBallCarrier <- trackingNew %>%
+  inner_join(runPlays, by = c("gameId", "playId")) %>%
+  filter(ballCarrierId == nflId) 
+
+### Making ball carrier tracking column names easier to identify for when this is merged with linebacker tracking data that have the same column names ###
+colnames(trackingBallCarrier)[c(14:17)] <- c("ballCarrierX", "ballCarrierY", "ballCarrierDir", "ballCarrierO")
+
+### Keep only columns that will be useful when merged to linebacker tracking data later ###
+trackingBallCarrier <- trackingBallCarrier %>%
+  dplyr::select(gameId, playId, frameId, ballCarrierX, ballCarrierY, ballCarrierDir, ballCarrierO, ballCarrierDisplayName, ballCarrierId)
+
+### Isolating linebackers tracking data to be merged with ball carrier tracking data later ###
+trackingLinebackers <- trackingNew %>%
+  inner_join(runPlays, by = c("gameId", "playId")) %>%
   inner_join(players, by = c("nflId", "displayName")) %>%
-  filter(position == "ILB" | position == "MLB") %>%
-  dplyr::select(nflId, displayName, xAdj, yAdj, gameId, playId) 
+  #filter(displayName != "football") %>%
+  #filter(club == defensiveTeam) %>%
+  filter(position == "MLB" | position == "ILB") %>%
+  left_join(tackles, by = c("gameId", "playId", "nflId"))
 
-### Merging data sets together to calculate linebacker distance to ball carrier at time of tackle ###
-tacklingNew <- tackling %>%
-  inner_join(ballCarrier, by = c("gameId", "playId")) %>%
-  mutate(distanceToBallCarrier = sqrt((xAdj - ballCarrierX)^2 + (yAdj - ballCarrierY)^2)) %>%
-  mutate(distLessThan2 = ifelse(distanceToBallCarrier < 2, 1, 0))
+### Making linebacker tracking column names easier to identify for when this is merged with ball carrier tracking data that have the same column names ###
+colnames(trackingLinebackers)[c(3, 4, 14:17)] <- c("defenderId", "defenderDisplayName", "defenderX", "defenderY", "defenderDir", "defenderO")
+
+### Keep only columns that will be useful when merged to ball carrier tracking data later ###
+trackingLinebackers <- trackingLinebackers %>%
+  dplyr::select(gameId, playId, frameId, defenderId, defenderDisplayName, defenderX, defenderY, defenderDir, defenderO, club, s, a, dis, event, tackle, assist, pff_missedTackle)
+trackingLinebackers <- replace(trackingLinebackers, is.na(trackingLinebackers), 0)
+
+### Adding ball carrier metrics to defender tracking data and calculating Eucledian distance of defender to ball carrier ###
+trackingPlays <- trackingLinebackers %>%
+  left_join(trackingBallCarrier, by = c("gameId", "playId", "frameId")) %>%
+  mutate(distToBallCarrier = sqrt((defenderX - ballCarrierX)^2 + (defenderY - ballCarrierY)^2))
+
+### finding linebackers not involved in the tackle and indicating if they were within 3 yards of ball carrier at time of tackle
+hustleDf <- trackingPlays %>%
+  filter(tackle == 0 & assist == 0) %>%
+  filter(event == "tackle") %>%
+  mutate(distLessThan3 = ifelse(distToBallCarrier <= 3, 1, 0))
 
 ### Calculate mean distance to ball carrier and percentage of time distance to ball carrier is less than 2 at time of tackle so a percentile ranking can be given ###
-df <- tacklingNew %>%
-  group_by(displayName) %>%
-  summarise(meanDistance = mean(distanceToBallCarrier), 
-            percentPlaysWithin2 = mean(distLessThan2), 
+### calculating percentile and rank/grade for each player based on their percentile
+df <- hustleDf %>%
+  group_by(defenderDisplayName) %>%
+  summarise(meanDistance = mean(distToBallCarrier), 
+            percentPlaysWithin3 = mean(distLessThan3), 
             totalPlays = n()) %>%
   filter(totalPlays > 25) %>%
-  dplyr::select(-totalPlays) %>%
-  mutate(percentileRank = ntile(percentPlaysWithin2, 100))
+  mutate(hustlePercentile = round(percent_rank(percentPlaysWithin3)*100, 0)) %>%
+  mutate(hustleGrade = case_when(
+    hustlePercentile > 90 & hustlePercentile <= 100 ~ 1,
+    hustlePercentile > 80 & hustlePercentile <= 90 ~ 2,
+    hustlePercentile > 70 & hustlePercentile <= 80 ~ 3,
+    hustlePercentile > 60 & hustlePercentile <= 70 ~ 4,
+    .default = 5
+  ))
